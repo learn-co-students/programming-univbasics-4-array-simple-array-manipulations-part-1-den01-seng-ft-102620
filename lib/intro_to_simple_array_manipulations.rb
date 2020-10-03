fdca89ef1d0ddb030afd4d44579bfe7ce9609088
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-03 12:38:12 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push (array, string)
+ newarray = array.push string
+ newarray
+end
+
+def using_unshift array, string
+  newarray = array.unshift string
+  newarray
+end
+
+def using_pop array
+  array.pop
+end
+
+def pop_with_args array
+  array.pop(2)
+end
+
+def using_shift array
+  array.shift 
+
+end
+
+def shift_with_args array
+  array.shift(2)
+  
+end
